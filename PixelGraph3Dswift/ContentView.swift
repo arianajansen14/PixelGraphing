e13a597f39613dd8eccc18b53fb2686a9a6abb51
arianajansen14 <arianajansen14@gmail.com>
2025-10-06 11:43:58 +0100

    Initial Commit

---
+++ PixelGraph3Dswift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixelGraph3Dswift
+//
+//  Created by ariana jansen on 06/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixelGraph3Dswift/PixelGraph3DswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelGraph3DswiftApp.swift
+//  PixelGraph3Dswift
+//
+//  Created by ariana jansen on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PixelGraph3DswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
